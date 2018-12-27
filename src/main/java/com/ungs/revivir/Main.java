d098967cc8af1ba85ca936673f9c1fbb8a89611b
Carlos <carloscaballeromorel@gmail.com>
2018-12-27 18:26:15 -0300

    coo.

--- src/main/java/com/ungs/revivir/Main.java
+++ src/main/java/com/ungs/revivir/Main.java
@@ -3,7 +3,7 @@ package com.ungs.revivir;
 import com.ungs.revivir.vista.ControladorPrincipal;
 
 public class Main {
-	// prueba 1
+	// prueba 2
 	public static void main(String[] args) {
 		new ControladorPrincipal();		
 	}
