56bc1288e8496b79626e25a39caad4b986f7dd5f
Carlos <carloscaballeromorel@gmail.com>
2018-12-27 20:03:12 -0300

    Commit para arreglar el ignore.

--- src/main/java/com/ungs/revivir/Main.java
+++ src/main/java/com/ungs/revivir/Main.java
@@ -3,7 +3,7 @@ package com.ungs.revivir;
 import com.ungs.revivir.vista.principal.ControladorPrincipal;
 
 public class Main {
-	// prueba 2
+	// prueba 3
 	public static void main(String[] args) {
 		new ControladorPrincipal();		
 	}
