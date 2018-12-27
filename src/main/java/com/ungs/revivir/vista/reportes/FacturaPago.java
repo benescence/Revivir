532cf2856d715f4ea27a6f090dde6c8d8042fb1f
Carlos <carloscaballeromorel@gmail.com>
2018-12-27 19:35:42 -0300

    Arreglos

--- src/main/java/com/ungs/revivir/vista/reportes/FacturaPago.java
+++ src/main/java/com/ungs/revivir/vista/reportes/FacturaPago.java
@@ -7,6 +7,15 @@ import java.util.Map;
 import com.ungs.revivir.persistencia.entidades.Pago;
 import com.ungs.revivir.vista.util.Formato;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
 
 
 
