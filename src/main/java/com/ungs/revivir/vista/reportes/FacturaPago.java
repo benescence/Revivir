13410a9e94803ca667aac2f048730597f6cb6023
BenitoMolinaX <benescence@hotmail.com>
2018-12-27 19:12:17 -0300

    commit

--- src/main/java/com/ungs/revivir/vista/reportes/FacturaPago.java
+++ src/main/java/com/ungs/revivir/vista/reportes/FacturaPago.java
@@ -7,14 +7,8 @@ import java.util.Map;
 import com.ungs.revivir.persistencia.entidades.Pago;
 import com.ungs.revivir.vista.util.Formato;
 
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.util.JRLoader;
-import net.sf.jasperreports.view.JasperViewer;
+
+
 
 public class FacturaPago {
 	private JasperReport reporte;
