e60aba5e07926c100494c94fba0f89be96cd04dd
Carlos <carloscaballeromorel@gmail.com>
2018-12-29 19:36:38 -0300

    Reporte test.

---
+++ src/main/java/com/ungs/revivir/test/reportes/ReporteTest.java
@@ -0,0 +1,17 @@
+package com.ungs.revivir.test.reportes;
+
+import java.util.List;
+
+import com.ungs.revivir.negocios.manager.PagoManager;
+import com.ungs.revivir.persistencia.entidades.Pago;
+import com.ungs.revivir.vista.reportes.FacturaPago;
+
+public class ReporteTest {
+
+	public static void main(String[] args) {
+		List<Pago> pagos = PagoManager.traerTodo();
+		FacturaPago reporte = new FacturaPago(pagos);
+		reporte.mostrar();
+	}
+
+}
