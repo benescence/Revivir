1acd168c5b6af854659cf7fe1f4e5491820f6767
BenitoMolinaX <benescence@hotmail.com>
2018-12-31 14:24:09 -0300

    reporte

---
+++ src/main/java/com/ungs/revivir/test/reportes/ReporteCajaTest.java
@@ -0,0 +1,18 @@
+package com.ungs.revivir.test.reportes;
+
+import java.util.List;
+
+import com.ungs.revivir.negocios.Almanaque;
+import com.ungs.revivir.negocios.manager.PagoManager;
+import com.ungs.revivir.persistencia.entidades.Pago;
+import com.ungs.revivir.vista.reportes.MovimientoDiario;
+
+public class ReporteCajaTest {
+
+	public static void main(String[] args) {
+		List<Pago> pagos = PagoManager.traerTodo();
+		MovimientoDiario reporte = new MovimientoDiario(Almanaque.hoy());
+		reporte.mostrar();
+	}
+
+}
