2b638fb5d0cda0dd9a81d1a4aa1ec91d64d5c306
Carlos <carloscaballeromorel@gmail.com>
2019-01-05 14:09:50 -0300

    Eliminar warning de reportes test.

--- src/main/java/com/ungs/revivir/test/reportes/ReporteMovimientosDiariosTest.java
+++ src/main/java/com/ungs/revivir/test/reportes/ReporteMovimientosDiariosTest.java
@@ -1,16 +1,12 @@
 package com.ungs.revivir.test.reportes;
 
-import java.sql.Date;
-
 import com.ungs.revivir.negocios.Almanaque;
 import com.ungs.revivir.vista.reportes.ReporteMovimientosDiarios;
 
 public class ReporteMovimientosDiariosTest {
 
 	public static void main(String[] args) {
-	
-		ReporteMovimientosDiarios reporte = new ReporteMovimientosDiarios(Almanaque.hoy());
-		
+		new ReporteMovimientosDiarios(Almanaque.hoy());
 	}
 
 }
