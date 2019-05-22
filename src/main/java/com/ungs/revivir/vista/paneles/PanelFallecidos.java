0ab342a8f366eaad4a7643215f480f9eb5ceb6d2
Carlos <carloscaballeromorel@gmail.com>
2019-05-21 21:55:04 -0300

    mejora busqueda de fallecido

---
+++ src/main/java/com/ungs/revivir/vista/paneles/PanelFallecidos.java
@@ -0,0 +1,84 @@
+package com.ungs.revivir.vista.paneles;
+
+import java.awt.Dimension;
+import java.util.List;
+
+import com.ungs.revivir.negocios.manager.FallecidoManager;
+import com.ungs.revivir.persistencia.entidades.Fallecido;
+import com.ungs.revivir.vista.util.Boton;
+import com.ungs.revivir.vista.util.Popup;
+import com.ungs.revivir.vista.util.TextoCentrado;
+import com.ungs.revivir.vista.util.contenedores.PanelVertical;
+import com.ungs.revivir.vista.util.entradas.EntradaNumero;
+import com.ungs.revivir.vista.util.entradas.EntradaTexto;
+
+public class PanelFallecidos extends PanelVertical {
+	private static final long serialVersionUID = 1L;
+	private EntradaTexto inNombre, inApellido;
+	private EntradaNumero inCodigo;
+	
+	public PanelFallecidos(Dimension dimTexto, Dimension dimEntrada, Dimension dimBoton) {
+		inNombre = new EntradaTexto("Nombres fallecido", dimTexto, dimEntrada);
+		inApellido = new EntradaTexto("Apellidos fallecido", dimTexto, dimEntrada);
+		inCodigo = new EntradaNumero("Cod. Fallecido", dimTexto, dimEntrada);
+		Boton btnCargar = new Boton("Cargar", dimBoton);
+		btnCargar.addActionListener(e-> {
+			autocompletar();
+		});
+		
+		add(new TextoCentrado("Datos del fallecido"));
+		add(inNombre);
+		add(inApellido);
+		add(inCodigo);
+		add(btnCargar);
+	}
+
+	private void autocompletar() {
+		// - Busca fallecidos con los datos ingresados
+		// - Si encuentra un unico fallecido lo coloca en el formulario
+		// - Si no encuentra ninguno lanza mensaje pidiendo llenar los campos correspondientes
+		// - Si encuentra demasiados abre ventana para seleccionar de una lista
+		
+		String nombre = inNombre.getValor();
+		String apellido = inApellido.getValor();
+		Integer codFallecido = inCodigo.getValor();
+		List<Fallecido> fallecidos = null;
+		
+		try {
+			fallecidos = FallecidoManager.traer(nombre, apellido, codFallecido);
+			if (fallecidos.size() == 0) {
+				Popup.mostrar("No existe ningun fallecido con los parametros ingresados");
+				
+			} else if (fallecidos.size() > 1) {
+				Popup.mostrar("Existen demasiados fallecidos con los parametros ingresados");
+				// mostrar ventana
+				
+			} else {
+				Fallecido unico = fallecidos.get(0);
+				inNombre.setValor(unico.getNombre());
+				inApellido.setValor(unico.getApellido());
+				inCodigo.setValor(unico.getCod_fallecido().toString());
+			}
+		
+		} catch (Exception e) {
+			Popup.mostrar(e.getMessage());
+		}
+		
+	}
+
+	
+	public EntradaTexto getNombre() {
+		return inNombre;
+	}
+	
+
+	public EntradaTexto getApellido() {
+		return inApellido;
+	}
+	
+
+	public EntradaNumero getCodigo() {
+		return inCodigo;
+	}
+	
+}
