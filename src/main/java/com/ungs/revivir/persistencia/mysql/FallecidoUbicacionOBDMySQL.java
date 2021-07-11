9b7bcd37c97aa7a00b7423d3db334d66e3577201
Ging1991 <carloscaballeromorel@gmail.com>
2021-07-11 19:30:32 -0300

    Fix vencimiento en NULL

--- src/main/java/com/ungs/revivir/persistencia/mysql/FallecidoUbicacionOBDMySQL.java
+++ src/main/java/com/ungs/revivir/persistencia/mysql/FallecidoUbicacionOBDMySQL.java
@@ -43,172 +43,13 @@ public class FallecidoUbicacionOBDMySQL extends OBD implements FallecidoUbicacio
 	}
 	
 	private List<FallecidoUbicacion> selectByCondicion(String condicion) {
-		List<FallecidoUbicacion> ret = new ArrayList<FallecidoUbicacion>();
 		String comandoSQL = "select ID, "+campos+" from "+tabla+" where ("+condicion+") limit "+limite+";";
-		
-		try { 
-			Class.forName(driver); 
-			Connection conexion = DriverManager.getConnection(cadenaConexion, usuarioBD, passwordBD); 
-			Statement sentencia = conexion.createStatement ();
-			ResultSet resultados = sentencia.executeQuery(comandoSQL);			
-
-			while (resultados.next()) {
-				Integer fila = resultados.getInt("fila");
-				fila = (resultados.wasNull())? null: fila;
-				
-				Integer nicho = resultados.getInt("nicho");
-				nicho = (resultados.wasNull())? null: nicho;
-
-				Integer macizo = resultados.getInt("macizo");
-				macizo = (resultados.wasNull())? null: macizo;
-				
-				Integer unidad = resultados.getInt("unidad");
-				unidad = (resultados.wasNull())? null: unidad;
-				
-				Integer sepultura = resultados.getInt("sepultura");
-				sepultura = (resultados.wasNull())? null: sepultura;
-				
-				Integer parcela = resultados.getInt("parcela");
-				parcela = (resultados.wasNull())? null: parcela;
-				
-				Integer mueble = resultados.getInt("mueble");
-				mueble = (resultados.wasNull())? null: mueble;
-				
-				Integer inhumacion = resultados.getInt("inhumacion");
-				inhumacion = (resultados.wasNull())? null: inhumacion;
-
-				Integer circ = resultados.getInt("circ");
-				circ = (resultados.wasNull())? null: circ;
-
-				Boolean bis = resultados.getBoolean("bis");
-				bis = (resultados.wasNull())? null: bis;
-
-				Boolean bisMacizo = resultados.getBoolean("bis_macizo");
-				bisMacizo = (resultados.wasNull())? null: bisMacizo;
-				
-				ret.add(new FallecidoUbicacion(
-						resultados.getInt("ID"),
-						resultados.getInt("ubicacion"),
-						Definido.tipoFallecimiento(resultados.getInt("tipo_fallecimiento")),
-						resultados.getInt("cod_fallecido"),
-						resultados.getString("cod_fallecido"), // revisar, deberia ir DNI
-						resultados.getString("apellido"),
-						resultados.getString("nombre"),
-						resultados.getString("cocheria"),
-						resultados.getDate("fecha_fallecimiento"),
-						resultados.getDate("fecha_ingreso"),
-						Definido.subsector(resultados.getInt("subsector")),
-						resultados.getString("cementerio"),
-						nicho,
-						fila,
-						resultados.getString("seccion"),
-						macizo,
-						unidad,
-						bis,
-						bisMacizo,
-						sepultura,
-						parcela,
-						mueble,
-						inhumacion,
-						circ,
-						Date.valueOf(resultados.getObject("vencimiento", LocalDate.class))
-						));
-			}
-			
-			resultados.close();
-			sentencia.close();
-			conexion.close();
-			
-		}catch(Exception e) {
-			System.out.println(comandoSQL);
-			e.printStackTrace();
-		}
-			
-		return ret;
+		return selectSQL(comandoSQL);
 	}
+	
 	private List<FallecidoUbicacion> selectByCondicion(String condicion, int limite) {
-		List<FallecidoUbicacion> ret = new ArrayList<FallecidoUbicacion>();
 		String comandoSQL = "select ID, "+campos+" from "+tabla+" where ("+condicion+") limit "+limite+";";
-		
-		try { 
-			Class.forName(driver); 
-			Connection conexion = DriverManager.getConnection(cadenaConexion, usuarioBD, passwordBD); 
-			Statement sentencia = conexion.createStatement ();
-			ResultSet resultados = sentencia.executeQuery(comandoSQL);			
-
-			while (resultados.next()) {
-				Integer fila = resultados.getInt("fila");
-				fila = (resultados.wasNull())? null: fila;
-				
-				Integer nicho = resultados.getInt("nicho");
-				nicho = (resultados.wasNull())? null: nicho;
-
-				Integer macizo = resultados.getInt("macizo");
-				macizo = (resultados.wasNull())? null: macizo;
-				
-				Integer unidad = resultados.getInt("unidad");
-				unidad = (resultados.wasNull())? null: unidad;
-				
-				Integer sepultura = resultados.getInt("sepultura");
-				sepultura = (resultados.wasNull())? null: sepultura;
-				
-				Integer parcela = resultados.getInt("parcela");
-				parcela = (resultados.wasNull())? null: parcela;
-				
-				Integer mueble = resultados.getInt("mueble");
-				mueble = (resultados.wasNull())? null: mueble;
-				
-				Integer inhumacion = resultados.getInt("inhumacion");
-				inhumacion = (resultados.wasNull())? null: inhumacion;
-
-				Integer circ = resultados.getInt("circ");
-				circ = (resultados.wasNull())? null: circ;
-
-				Boolean bis = resultados.getBoolean("bis");
-				bis = (resultados.wasNull())? null: bis;
-
-				Boolean bisMacizo = resultados.getBoolean("bis_macizo");
-				bisMacizo = (resultados.wasNull())? null: bisMacizo;
-				
-				ret.add(new FallecidoUbicacion(
-						resultados.getInt("ID"),
-						resultados.getInt("ubicacion"),
-						Definido.tipoFallecimiento(resultados.getInt("tipo_fallecimiento")),
-						resultados.getInt("cod_fallecido"),
-						resultados.getString("cod_fallecido"), // revisar, deberia ir DNI
-						resultados.getString("apellido"),
-						resultados.getString("nombre"),
-						resultados.getString("cocheria"),
-						resultados.getDate("fecha_fallecimiento"),
-						resultados.getDate("fecha_ingreso"),
-						Definido.subsector(resultados.getInt("subsector")),
-						resultados.getString("cementerio"),
-						nicho,
-						fila,
-						resultados.getString("seccion"),
-						macizo,
-						unidad,
-						bis,
-						bisMacizo,
-						sepultura,
-						parcela,
-						mueble,
-						inhumacion,
-						circ,
-						Date.valueOf(resultados.getObject("vencimiento", LocalDate.class))
-						));
-			}
-			
-			resultados.close();
-			sentencia.close();
-			conexion.close();
-			
-		}catch(Exception e) {
-			System.out.println(comandoSQL);
-			e.printStackTrace();
-		}
-			
-		return ret;
+		return selectSQL(comandoSQL);
 	}
 	
 	public FallecidoUbicacion selectByID(Integer ID) {
@@ -231,6 +72,95 @@ public class FallecidoUbicacionOBDMySQL extends OBD implements FallecidoUbicacio
 		String condicion = "subsector = "+Definido.subsector(subSector)
 			+ " and vencimiento between '"+desde+"' and '"+hasta+"'";
 		return selectByCondicion(condicion, 1000000);
+	}
+	
+	// METODOS PRIVADOS
+	
+	private List<FallecidoUbicacion> selectSQL(String SQL) {
+		List<FallecidoUbicacion> ret = new ArrayList<FallecidoUbicacion>();
+		
+		try { 
+			Class.forName(driver); 
+			Connection conexion = DriverManager.getConnection(cadenaConexion, usuarioBD, passwordBD); 
+			Statement sentencia = conexion.createStatement ();
+			ResultSet resultados = sentencia.executeQuery(SQL);			
+
+			while (resultados.next()) {
+				Integer fila = resultados.getInt("fila");
+				fila = (resultados.wasNull())? null: fila;
+				
+				Integer nicho = resultados.getInt("nicho");
+				nicho = (resultados.wasNull())? null: nicho;
+
+				Integer macizo = resultados.getInt("macizo");
+				macizo = (resultados.wasNull())? null: macizo;
+				
+				Integer unidad = resultados.getInt("unidad");
+				unidad = (resultados.wasNull())? null: unidad;
+				
+				Integer sepultura = resultados.getInt("sepultura");
+				sepultura = (resultados.wasNull())? null: sepultura;
+				
+				Integer parcela = resultados.getInt("parcela");
+				parcela = (resultados.wasNull())? null: parcela;
+				
+				Integer mueble = resultados.getInt("mueble");
+				mueble = (resultados.wasNull())? null: mueble;
+				
+				Integer inhumacion = resultados.getInt("inhumacion");
+				inhumacion = (resultados.wasNull())? null: inhumacion;
+
+				Integer circ = resultados.getInt("circ");
+				circ = (resultados.wasNull())? null: circ;
+
+				Boolean bis = resultados.getBoolean("bis");
+				bis = (resultados.wasNull())? null: bis;
+
+				Boolean bisMacizo = resultados.getBoolean("bis_macizo");
+				bisMacizo = (resultados.wasNull())? null: bisMacizo;
+				
+				LocalDate vencimientoLocal = (LocalDate) resultados.getObject("vencimiento", LocalDate.class);
+				Date vencimiento = vencimientoLocal == null ? null : Date.valueOf(vencimientoLocal);
+				
+				ret.add(new FallecidoUbicacion(
+						resultados.getInt("ID"),
+						resultados.getInt("ubicacion"),
+						Definido.tipoFallecimiento(resultados.getInt("tipo_fallecimiento")),
+						resultados.getInt("cod_fallecido"),
+						resultados.getString("cod_fallecido"), // revisar, deberia ir DNI
+						resultados.getString("apellido"),
+						resultados.getString("nombre"),
+						resultados.getString("cocheria"),
+						resultados.getDate("fecha_fallecimiento"),
+						resultados.getDate("fecha_ingreso"),
+						Definido.subsector(resultados.getInt("subsector")),
+						resultados.getString("cementerio"),
+						nicho,
+						fila,
+						resultados.getString("seccion"),
+						macizo,
+						unidad,
+						bis,
+						bisMacizo,
+						sepultura,
+						parcela,
+						mueble,
+						inhumacion,
+						circ,
+						vencimiento
+						));
+			}
+			
+			resultados.close();
+			sentencia.close();
+			conexion.close();
+			
+		}catch(Exception e) {
+			System.out.println(SQL);
+			e.printStackTrace();
+		}
+			
+		return ret;
 	}
 
 }
