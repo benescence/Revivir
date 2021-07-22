b6c888d5f12ba4f585f2ad0aa5c25f08e8d0d85c
Ging1991 <carloscaballeromorel@gmail.com>
2021-07-22 18:52:01 -0300

    prueba reportes

--- src/main/java/com/ungs/revivir/test/reportes/ReporteMovimientosDiariosTest.java
+++ src/main/java/com/ungs/revivir/test/reportes/ReporteMovimientosDiariosTest.java
@@ -1,12 +1,15 @@
 package com.ungs.revivir.test.reportes;
 
-import com.ungs.revivir.negocios.Almanaque;
+import java.sql.Date;
+import java.time.LocalDate;
+
 import com.ungs.revivir.vista.reportes.ReporteMovimientosDiarios;
 
 public class ReporteMovimientosDiariosTest {
 
 	public static void main(String[] args) {
-		new ReporteMovimientosDiarios(Almanaque.hoy());
+		Date fecha = Date.valueOf(LocalDate.of(2019, 8, 10));
+		new ReporteMovimientosDiarios(fecha);
 	}
 
 }
--- src/main/java/com/ungs/revivir/vista/reportes/ReporteMovimientosDiarios.java
+++ src/main/java/com/ungs/revivir/vista/reportes/ReporteMovimientosDiarios.java
@@ -6,11 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.ungs.revivir.negocios.manager.PagoManager;
 import com.ungs.revivir.negocios.manager.ReportePagoManager;
-import com.ungs.revivir.persistencia.entidades.Pago;
 import com.ungs.revivir.persistencia.entidades.ReportePago;
-import com.ungs.revivir.vista.util.Formato;
 import com.ungs.revivir.vista.util.Popup;
 
 import net.sf.jasperreports.engine.JRException;
@@ -27,48 +24,48 @@ public class ReporteMovimientosDiarios {
 	private JasperPrint reporteLleno;
 	
 	public ReporteMovimientosDiarios(Date fecha) {
-		Map<String, Object> totalPagos = new HashMap<String, Object>();
-		List<String> servicios = new ArrayList<String>();
-		//List<String> clientes = new ArrayList<String>();
-		List<Double> montos = new ArrayList<Double>();
+		List<ReportePago> pagos = ReportePagoManager.traerPorFecha(fecha);
+		Map<String, Object> parametros = new HashMap<String, Object>();
+		List<String> itemServicios = new ArrayList<String>();
+		List<String> itemClientes = new ArrayList<String>();
+		List<Double> itemsMontos = new ArrayList<Double>();
 		Double suma = 0.0;
 		List<Double> total = new ArrayList<Double>();
-		//List<Pago> pagos = PagoManager.traerPorFecha(fecha);
 		
-		List<ReportePago> pagos = ReportePagoManager.traerPorFecha(fecha);
-		/*
-		for (Pago pago : pagos) {
-			//String nombreCliente = ClienteManager.traerPorID(pago.getCliente()).getNombre();
-			//String apellidoCliente = ClienteManager.traerPorID(pago.getCliente()).getApellido();
-			//clientes.add(nombreCliente);
-			//clientes.add(apellidoCliente);
-			servicios.add(Formato.servicioDesdeCargo(pago));
-			montos.add(pago.getImporte());
-			suma= suma + pago.getImporte();
+		for (ReportePago pago : pagos) {
+			String nombreCliente = pago.getNombre();
+			String apellidoCliente = pago.getApellido();
+			itemClientes.add(nombreCliente);
+			itemClientes.add(apellidoCliente);
+			itemServicios.add(pago.getServicioNombre());
+			itemsMontos.add(new Double(pago.getImporte()));
+			suma = suma + pago.getImporte();
 		}
-		*/
-		if (pagos.size() != 0) {
-		
-		total.add(suma);
-		totalPagos.put("descripcion", servicios);
-		//totalPagos.put("cliente", clientes);
-		totalPagos.put("monto", montos);
-		totalPagos.put("total", total);
-		
-		try {
+		 
+		if (pagos.size() > 0) {
+			
+			total.add(suma);
+			parametros.put("Fecha", fecha);
+			parametros.put("descripcion", itemServicios);
+			parametros.put("cliente", itemClientes);
+			parametros.put("monto", itemsMontos);
+			parametros.put("total", total);
+			
+			try {
 				this.reporte = (JasperReport) JRLoader.loadObjectFromFile("reportes\\MovimientoDiario.jasper");
-				this.reporteLleno = JasperFillManager.fillReport(this.reporte, totalPagos,
+				this.reporteLleno = JasperFillManager.fillReport(this.reporte, parametros,
 						new JRBeanCollectionDataSource(pagos));
 				System.out.println("Se cargo correctamente reporte");
 				mostrar();
-		
-		} catch (JRException ex) {
-			System.out.println("Ocurrio un error mientras se cargaba el archivo movimientos diario.Jasper \n " + ex);
-		}
-		}
-		else {
+			
+			} catch (JRException ex) {
+				System.out.println("Ocurrio un error mientras se cargaba el archivo movimientos diario.Jasper \n " + ex);
+			}
+
+		} else {
 			Popup.mostrar("No hay pagos para la fecha seleccionada");
 		}
+
 	}
 
 	private void mostrar() {
